ed10e74147cdb39e0f97fa9b106c2ec531ed8d92
Martin <Martin@Martin>
2023-06-28 17:48:29 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,19 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+
+    grades = Student.find_by(id: params[:id])
+    render json: grades
+
+  end
+
+
+  def index
+    name = params[:name]
+    @students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+    render json: @students
+  end
+
+
 end
